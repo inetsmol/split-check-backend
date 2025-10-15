b99c23338150e32c6cf6c563b52254bad3d9c8c8
big <inetsmol@gmail.com>
2025-10-15 13:22:05 +0300

    авторизация по гугл токену

--- src/api/v1/endpoints/token.py
+++ src/api/v1/endpoints/token.py
@@ -1,18 +1,83 @@
-from typing import Dict
+import logging
+import uuid
+from typing import Dict, Optional
 
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import JWTError
 from starlette.requests import Request
 
+from fastapi import Body
+from firebase_admin import auth
+
 from src.config import config
 from src.core.security import verify_token
-from src.schemas import RefreshTokenRequest, TokenResponse
+from src.repositories.user import get_user_by_email, create_new_user
+from src.schemas import RefreshTokenRequest, TokenResponse, UserCreate
 from src.services.auth import authenticate_user, generate_tokens
+
+logger = logging.getLogger(config.app.service_name)
 
 router = APIRouter()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/token")
+
+
+@router.post(
+    "/google",
+    summary="Авторизация по Google ID Token",
+    response_model=TokenResponse,
+    status_code=status.HTTP_200_OK,
+    responses={
+        400: {"description": "Bad request"},
+        401: {"description": "Invalid Google ID token"},
+        404: {"description": "User not found and cannot be created"},
+    },
+)
+async def login_google_token(id_token: str, lang: Optional[str] = "en"):
+    """
+    Принимает **Google ID Token** (OIDC), валидирует его и возвращает нашу пару токенов.
+    """
+    logger.debug("Получен Google id_token (обрезано в логах)")
+
+    try:
+
+        claims = auth.verify_id_token(id_token)
+        email = claims.get('email')
+        logger.debug(f"user: {claims}")
+
+        user = await get_user_by_email(email)
+        if not user:
+            # Создаем нового пользователя
+            user = await create_new_user(
+                user_data=UserCreate(
+                    email=email,
+                    password=uuid.uuid4().hex
+                ),
+                profile_data={
+                    "nickname": claims.get("name"),
+                    "language": lang,
+                    "avatar_url": claims.get('picture')
+                }
+            )
+        tokens = await generate_tokens(user.email, user.id)
+
+        return tokens
+
+    except ValueError as e:
+        # Ошибка верификации токена
+        logger.error(f"Invalid token: {e}")
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid token"
+        )
+    except Exception as e:
+        # Логируем неожиданные ошибки
+        logger.error(f"Unexpected error during Google authentication: {str(e)}")
+        raise HTTPException(
+            status_code=500,
+            detail="Authentication failed"
+        )
 
 
 # Эндпоинт для получения access_token и refresh_token
